443d0ded8c8f2b8dfdc9b0d9a9b32e1fba66fde7
Yu-Cheng, Chang <45489725+Yucheng0208@users.noreply.github.com>
2025-02-23 00:02:55 +0800

    Create Ch02.py

---
+++ Ch02.py
@@ -0,0 +1,19 @@
+# Q3
+for i in range(1, 6):
+    print("*" * i)
+
+# Q4
+math = 75
+english = 68
+total = math + english
+print("總分:", total)
+
+# Q5
+print("大家好!")
+print(250)
+print("\\200")
+
+#Q6
+values = [0o277, 0xcc, 0xab, 0o333, 0o555, 0xff]
+for v in values:
+    print(f"十進位: {v}")
